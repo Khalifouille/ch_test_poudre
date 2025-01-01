14920783e90ded813e5e18a010debc46b0731dce
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-01 15:45:18 +0100

    detected shot + reset function

--- server.lua
+++ server.lua
@@ -5,4 +5,14 @@ local playerShooters = {}
 RegisterNetEvent('playerShotWeapon')
 AddEventHandler('playerShotWeapon', function(weaponHash)
     -- print("Test crampté 1")
+    local playerId = source
+    print("Crampté au nom de " .. tostring(playerId) .. " a tiré")
+
+    playerShooters[playerId] = true
+
+    Citizen.CreateThread(function()
+        Citizen.Wait(5000)
+        playerShooters[playerId] = nil
+        print("Reset fait")
+    end)
 end)
