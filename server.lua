7f61e93319c293eed2c800ffc114404fd56f93f8
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-02 14:42:45 +0100

    modifs leg

--- server.lua
+++ server.lua
@@ -1,5 +1,3 @@
-print("Tu marche?")
-
 ESX = exports['es_extended']:getSharedObject()
 
 local playerShooters = {}
@@ -25,12 +23,13 @@ AddEventHandler('checktir', function(targetPlayerId)
     if xPlayer.getInventoryItem('test_de_poudre').count > 0 then
         if playerShooters[targetPlayerId] then
             xPlayer.removeInventoryItem('test_de_poudre', 1)
-            TriggerClientEvent('chat:addMessage', sourcePlayerId, { args = { '^1SYSTEM', 'Le joueur a tiré récemment. Test de poudre utilisé.' } })
+            TriggerClientEvent('chat:addMessage', sourcePlayerId, { args = { '^1SYSTEM', ' Le joueur a tiré récemment. Test de poudre utilisé.' } })
         else
-            TriggerClientEvent('chat:addMessage', sourcePlayerId, { args = { '^1SYSTEM', 'Le joueur n\'a pas tiré récemment.' } })
+            TriggerClientEvent('chat:addMessage', sourcePlayerId, { args = { '^1SYSTEM', ' Le joueur n\'a pas tiré récemment.' } })
+            xPlayer.removeInventoryItem('test_de_poudre', 1)
         end
     else
-        TriggerClientEvent('chat:addMessage', sourcePlayerId, { args = { '^1SYSTEM', 'Vous avez besoin d\'un test de poudre.' } })
+        TriggerClientEvent('chat:addMessage', sourcePlayerId, { args = { '^1SYSTEM', ' Vous avez besoin d\'un test de poudre.' } })
     end
 end)
 
